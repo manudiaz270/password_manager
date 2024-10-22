1906f42d254820d3e0ee8ac1a647f3b3ce9e36ea
Manuel <damanuel2007@gmail.com>
2024-10-22 15:55:45 -0300

    added way to look at passwords

--- main.py
+++ main.py
@@ -2,14 +2,36 @@
 #poder agregar contraseñas o pedirlas, utilizando una contraseña maestra
 #mantenerse entre sesiones
 import csv
+from cryptography.fernet import Fernet
+import ast
+key = b'Z1MPWdmwabH551KvAu6LAodnA5u3G6KJNFgiJeQQvMI='
 
-data = [
-    {'account': 'instagram', 'name': 'manu', 'password': 'aflewybfalye'},
-    {'account': 'youtube', 'name': 'manucho', 'password': 'kajsbdlfhaadf'}
-]
+def add_password(key):
+    account = input('account: ')
+    name = input('name: ')
+    password = input('password: ')
+    fernet = Fernet(key)
+    encoded_password = fernet.encrypt(password.encode())
+    data = {'account': account, 'name': name, 'password': encoded_password}
 
-with open('passwords.csv', 'w', newline='') as passwords:
-    fieldnames = ['account', 'name', 'password']
-    writer = csv.DictWriter(passwords, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(data) 
+    with open('passwords.csv', 'a', newline='') as passwords:
+        feildnames = ['account', 'name', 'password']
+        writer = csv.DictWriter(passwords, fieldnames=feildnames)
+        writer.writerow(data)
+
+def list_passwords(key):
+    with open('passwords.csv', mode='r') as passwords:
+        passwords_file = csv.reader(passwords)
+        decrypted_passwords = []
+        for lines in passwords_file:
+            decrypted_passwords.append(lines)
+        decrypted_passwords.pop(0)
+        fernet = Fernet(key)
+        print(key)
+        
+        for i in range(len(decrypted_passwords)):
+            password = ast.literal_eval(decrypted_passwords[i][2])
+            decrypted_passwords[i][2] = fernet.decrypt(password).decode()
+        print(decrypted_passwords)
+
+list_passwords(key)
